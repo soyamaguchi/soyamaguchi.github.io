8db1beb2572350d994c222285c1acf85a0ea7e2a
Soya Yamaguchi <f.soya.maguchi@gmail.com>
2019-09-08 23:22:13 +0900

    worksページの実装途中

---
+++ src/components/Achievement.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="achievement">
+    <h1>Products</h1>
+    <div class="products-info">
+      <span class="date">2019-09</span>
+      <h2>PortfolioSite - soyamaguchi.me</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Achievement.vue"
+  }
+</script>
+
+<style lang="scss" scoped>
+.achievement {
+  h1 {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    color: #737373;
+  }
+  .date {
+    color: #c2c2c2;
+  }
+  h2 {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    color: #616161;
+  }
+}
+</style>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,15 +1,22 @@
 <template>
-    <div>
-
+    <div class="works">
+        <Achievement />
     </div>
 </template>
 
 <script>
-    export default {
-        name: "Works.vue"
-    }
+import Achievement from '@/components/Achievement.vue'
+
+export default {
+  name: "Works.vue",
+  components: {
+      Achievement
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-
+.works {
+    margin-top: 5rem;
+}
 </style>
