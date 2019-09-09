ef48380ced5f79ed3ff2884e0d9e6bc380e8061e
Soya Yamaguchi <f.soya.maguchi@gmail.com>
2019-09-09 23:07:59 +0900

    Add worksページのコンテンツ

--- src/components/Achievement.vue
+++ src/components/Achievement.vue
@@ -4,6 +4,14 @@
     <div class="products-info">
       <span class="date">2019-09</span>
       <h2>PortfolioSite - soyamaguchi.me</h2>
+      <p>個人開発</p>
+      <a href="https://soyamaguchi.me" target="_blank" rel="noopener">soyamaguchi.me</a>
+    </div>
+    <div class="products-info">
+      <span class="date">2019-08</span>
+      <h2>知っておいて損はないお得な自治体情報 - Advice2.com</h2>
+      <p>個人開発</p>
+      <a href="https://advice2.com" target="_blank" rel="noopener">advice2.com</a>
     </div>
   </div>
 </template>
@@ -15,17 +23,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$font-fam: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+
 .achievement {
   h1 {
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    color: #737373;
+    margin: 0 0 2rem;
+    font-family: $font-fam;
+    color: #808080;
   }
-  .date {
-    color: #c2c2c2;
+  .products-info {
+    margin-bottom: 2rem;
+    .date {
+      color: #c2c2c2;
+    }
+    h2 {
+      margin: 0;
+      font-family: $font-fam;
+      color: #616161;
+    }
+    p,
+    a {
+      color: #474747;
+    }
+    p {
+      margin: 0.5rem 0;
+    }
   }
-  h2 {
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    color: #616161;
-  }
+
 }
 </style>
