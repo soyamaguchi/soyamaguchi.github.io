0c5b4d0e7283b6facdcfdcfb035ddd63993a8ff3
Soya Yamaguchi <f.soya.maguchi@gmail.com>
2019-09-13 23:41:47 +0900

    Add ogp

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,21 @@ export default {
    ** Headers of the page
    */
   head: {
+    htmlAttrs: {
+      prefix: 'og: http://ogp.me/ns#'
+    },
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@_soyamaguchi_' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'soyamaguchi.me' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://soyamaguchi.me' },
+      { hid: 'og:title', property: 'og:title', content: 'soyamaguchi.me' },
+      { hid: 'og:image', property: 'og:image', content: 'https://soyamaguchi.me/portfolio-ogp.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
